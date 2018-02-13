e1c19cbc441444e8a20df43c66773c56ef13d203
Karl-Kaspar Kuleša <karlkasparkulesha@gmail.com>
2018-02-13 22:05:40 +0200

    Basic step changing logic implemented

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,3 +1,32 @@
 $(window).ready(function(){
   console.log("custom.js");
+  changeStep = function (elem, way) {
+    console.log("changeStep()");
+    var currentStep = "";
+    var stepWanted = "";
+    if (way == "indicator") {
+      //console.log("Clicked on numeric indicator");
+      currentStep = $(".step.active").attr("id");
+      stepWanted = $(elem).parent().attr("idStep");
+      console.log("Current step: " + currentStep);
+      console.log("Wanted step: " + stepWanted);
+      if (validator(currentStep, stepWanted) == true)
+      {
+        $(".step").removeClass("active");
+        $("#mainForm").find("#"+stepWanted).addClass("active");
+      }
+      else {
+        // Error
+      }
+    }
+    else if (way == "forwardBtn") {
+      //console.log("Clicked on forward button");
+      currentStep = $(".step.active").attr("id");
+      //console.log("Current step: " + currentStep);
+    }
+  };
+  validator = function (currentStep, stepWanted) {
+    console.log("validator()");
+    return true;
+  };
 });
